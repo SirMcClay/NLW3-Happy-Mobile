e2df7bfd239d7eaa1d8bf9a793852d80de2c4285
sirmcclay <mcclay.design@gmail.com>
2020-10-17 12:12:58 -0300

    Create orphanage pages navigation templates, route and initial config

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -10,7 +10,11 @@ export default function OrphanagesMap() {
   const navigation = useNavigation();
 
   function handleNavigateToOrphanageDetails() {
-    navigation.navigate('OrphanageDetails')
+    navigation.navigate('OrphanageDetails');
+  }
+
+  function handleNavigateToCreateOrphanage() {
+    navigation.navigate('SelectMapPosition');
   }
 
   return (    <View style={styles.container}>
@@ -48,7 +52,7 @@ export default function OrphanagesMap() {
     <View style={styles.footer}>
       <Text style={styles.footerText}>2 orfanatos encontrados</Text>
 
-      <TouchableOpacity style={styles.createOrphanageButton} onPress={() => {}}>
+      <TouchableOpacity style={styles.createOrphanageButton} onPress={handleNavigateToCreateOrphanage}>
         <Feather name="plus" size={20} color="#FFF" />
       </TouchableOpacity>
     </View>
